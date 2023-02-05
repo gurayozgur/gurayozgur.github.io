9545f0765c79a8eddbf999f5c2621969c5ada13b
Guray Ozgur <guray.ozgur@metu.edu.tr>
2023-02-05 01:29:44 +0100

    Update add_timestamp_and_location.py

--- reflection-avenue/add_timestamp_and_location.py
+++ reflection-avenue/add_timestamp_and_location.py
@@ -6,24 +6,24 @@ def add_timestamp_and_location():
     if q == "url":
         img_url = input("Enter the URL of the image: ")
         import urllib.request
-        urllib.request.urlretrieve(img_url,"image_with_timestamp.jpg")
-        image = Image.open("image_with_timestamp.jpg")
+        urllib.request.urlretrieve(img_url,"retrieved_image.jpg")
+        image = Image.open("retrieved_image.jpg")
     else:
         img_pth = input("Enter the path of the image: ")
         image = Image.open(img_pth)
     image = crop_center_square(image)
-    # Resize image to 1080x1080
-    image = image.resize((1080, 1080))
+    # Resize image to 3600x3600
+    image = image.resize((3600, 3600))
     draw = ImageDraw.Draw(image)
     # Use Lato Light 300 font
-    font = ImageFont.truetype('LatoTR-Light.ttf', size=40)
+    font = ImageFont.truetype('LatoTR-Light.ttf', size=133)
     # Get width and height of the image
     width, height = image.size
     # Measure the size of the text
     timestamp_size = draw.textsize(timestamp, font)
     location_size = draw.textsize(location, font)
     # Add timestamp and location to the bottom right corner
-    draw.text((width - timestamp_size[0]-location_size[0]-30, height - 60), timestamp+", "+location, font=font, fill=(255, 255, 255))
+    draw.text((width - timestamp_size[0]-location_size[0]-100, height - 200), timestamp+", "+location, font=font, fill=(255, 255, 255))
     # Save image
     image.save(timestamp+"_"+location+'.jpg')
 
